a0c5a9afb692b99b01601b055cd87d22fa4f0c40
Karthik-0704 <karthikultimate07@gmail.com>
2025-02-12 00:39:20 -0700

    train

--- train.py
+++ train.py
@@ -5,4 +5,5 @@ os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'
 
 if __name__ == '__main__':
     model = YOLO("yolo11m.pt")
+    print("")
     model.train(data = "./data.yaml", imgsz = 640, batch = 8, epochs = 100)
